1c65a71d2db767e655f7c5bf80a496c9ac20fe0a
Your Name <you@example.com>
2019-03-11 16:48:17 +0400

    change routes

--- app/routes.php
+++ app/routes.php
@@ -1,43 +1,48 @@
 <?php
 
     /**
-     * Index
+     * API Routes
      */
-    $app->map(['GET', 'POST'], '/', function($request, $response, $args) {
-        return $response->withJson([
-            'appName'    => getenv('APP_NAME'),
-            'appVersion' => getenv('APP_VERSION')
-        ]);
-    });
-
-    /**
-     * AUTH Routes
-     */
-    $app->group('/auth', function() {
-        // sign in
-        $this->post('/signin', '\App\Controllers\UserController:signIn');
-        // sign up
-        $this->post('/signup', '\App\Controllers\UserController:signUp');
-    });
-
-    /**
-     * USER Routes
-     */
-    $app->group('/user', function() {
-        // get user
-        $this->post('/info', '\App\Controllers\UserController:getUser');
-        // update user
-        $this->post('/update', '\App\Controllers\UserController:updateUser');
-    });
-
-    /**
-     * GAME Routes
-     */
-    $app->group('/game', function() {
-        // get default
-        $this->post('/default', '\App\Controllers\GameController:getDefault');
-        // insert
-        $this->post('/insert', '\App\Controllers\GameController:insertGame');
+    $app->group('/api', function() {
+        /**
+         * Index
+         */
+        $this->map(['GET', 'POST'], '', function($request, $response, $args) {
+            return $response->withJson([
+                'appName'    => getenv('APP_NAME'),
+                'appVersion' => getenv('APP_VERSION')
+            ]);
+        });
+    
+        /**
+         * AUTH Routes
+         */
+        $this->group('/auth', function() {
+            // sign in
+            $this->post('/signin', '\App\Controllers\UserController:signIn');
+            // sign up
+            $this->post('/signup', '\App\Controllers\UserController:signUp');
+        });
+    
+        /**
+         * USER Routes
+         */
+        $this->group('/user', function() {
+            // get user
+            $this->post('/info', '\App\Controllers\UserController:getUser');
+            // update user
+            $this->post('/update', '\App\Controllers\UserController:updateUser');
+        });
+    
+        /**
+         * GAME Routes
+         */
+        $this->group('/game', function() {
+            // get default
+            $this->post('/default', '\App\Controllers\GameController:getDefault');
+            // insert
+            $this->post('/insert', '\App\Controllers\GameController:insertGame');
+        });
     });
 
     /**
