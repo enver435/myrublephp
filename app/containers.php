79fe8d1df9cec14e750d0b2a90cc0bc631ab4302
Your Name <you@example.com>
2019-04-22 21:28:35 +0400

    error handler disable

--- app/containers.php
+++ app/containers.php
@@ -5,19 +5,19 @@
     $container = $app->getContainer();
 
     // exception handler
-    $container['errorHandler'] = function($container) {
-        return function($request, $response, $exception) use($container) {
-            $data = [
-                'status'  => false,
-                'code'    => $exception->getCode(),
-                'message' => $exception->getMessage(),
-                'file'    => $exception->getFile(),
-                'line'    => $exception->getLine(),
-                'trace'   => explode("\n", $exception->getTraceAsString()),
-            ];
-            return $response->withJson($data);
-        };
-    };
+    // $container['errorHandler'] = function($container) {
+    //     return function($request, $response, $exception) use($container) {
+    //         $data = [
+    //             'status'  => false,
+    //             'code'    => $exception->getCode(),
+    //             'message' => $exception->getMessage(),
+    //             'file'    => $exception->getFile(),
+    //             'line'    => $exception->getLine(),
+    //             'trace'   => explode("\n", $exception->getTraceAsString()),
+    //         ];
+    //         return $response->withJson($data);
+    //     };
+    // };
 
     // page not found handler
     $container['notFoundHandler'] = function ($container) {
