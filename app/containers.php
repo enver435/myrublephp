464d6a3939683e9dfc02c2ead08c55e2c5fff1af
Your Name <you@example.com>
2019-04-22 22:55:45 +0400

    view global variables move base controller

--- app/containers.php
+++ app/containers.php
@@ -1,7 +1,5 @@
 <?php
 
-    use App\Models\BaseModel;
-
     $container = $app->getContainer();
 
     // exception handler
@@ -50,27 +48,20 @@
         // set view directory
         $view = new \Slim\Views\Twig($viewDir);
         
+        // add view extenstions
         $router = $container->get('router');
         $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
         $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
         $view->addExtension(new \Twig_Extension_Debug());
         $view->addExtension(new \App\System\Extensions\TwigExtension($container));
-
-        // all view render
-        $view['siteName'] = getenv('APP_NAME');
-        $view['baseUrl']  = getenv('APP_URL');
-        $view['fullUrl']  = $container->get('request')->getUri();
-        $view['session']  = $_SESSION;
-        $view['cookie']   = $_COOKIE;
-        $view['env']      = ENVIRONMENT;
-
-        // header withdraws count
-        $view['headerWithdraws'] = [
-            'all'      => BaseModel::count('withdraws'),
-            'waiting'  => BaseModel::count('withdraws', ['payment_status' => 0]),
-            'paid'     => BaseModel::count('withdraws', ['payment_status' => 1]),
-            'not_paid' => BaseModel::count('withdraws', ['payment_status' => 2])
-        ];
+        
+        // add view global variables
+        $view->getEnvironment()->addGlobal('siteName', getenv('APP_NAME'));
+        $view->getEnvironment()->addGlobal('baseUrl', getenv('APP_URL'));
+        $view->getEnvironment()->addGlobal('fullUrl', $container->get('request')->getUri());
+        $view->getEnvironment()->addGlobal('session', $_SESSION);
+        $view->getEnvironment()->addGlobal('cookie', $_COOKIE);
+        $view->getEnvironment()->addGlobal('env', ENVIRONMENT);
 
         return $view;
     };
--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -2,6 +2,7 @@
 
     namespace App\Controllers;
     use \Psr\Container\ContainerInterface as Container;
+    use App\Models\BaseModel;
 
     class BaseController
     {
@@ -10,6 +11,20 @@
         public function __construct(Container $container)
         {
             $this->container = $container;
+
+            // add view global variables
+            $this->addGlobalView();
+        }
+
+        private function addGlobalView()
+        {
+            // header withdraws count
+            $this->container->view->getEnvironment()->addGlobal('headerWithdraws', [
+                'all'      => BaseModel::count('withdraws'),
+                'waiting'  => BaseModel::count('withdraws', ['payment_status' => 0]),
+                'paid'     => BaseModel::count('withdraws', ['payment_status' => 1]),
+                'not_paid' => BaseModel::count('withdraws', ['payment_status' => 2])
+            ]);
         }
 
         public function __get($property)
