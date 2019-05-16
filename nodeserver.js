a8e4dac5f7fcbbe5a4b9d12f12e7f029d5bb23a8
Your Name <you@example.com>
2019-05-16 14:07:14 +0400

    added get mac

--- nodeserver.js
+++ nodeserver.js
@@ -1,6 +1,25 @@
-const find = require('local-devices');
- 
-// Find all local network devices.
-find().then(devices => {
-  console.log(devices);
-})
+// const find = require('local-devices');
+
+// // Find all local network devices.
+// find().then(devices => {
+//   console.log(devices);
+// })
+
+// const netList = require('network-list');
+
+// netList.scanEach({}, (err, obj) => {
+//     console.log(obj); // device object
+// });
+
+const getmac = require('getmac');
+const http = require('http');
+
+// create server
+http.createServer(function (req, res) {
+    getmac.getMac(function (err, macAddress) {
+        if (err) throw err
+
+        res.write('Mac Address: ' + macAddress);
+        res.end();
+    });
+}).listen(8080);
