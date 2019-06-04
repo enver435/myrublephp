d7081a6ca85b98d3c8f015afb1e32295839924a8
Your Name <you@example.com>
2019-06-04 21:05:38 +0400

    change env

--- app/routes/site.php
+++ app/routes/site.php
@@ -13,28 +13,28 @@
      * Yandex OAuth
      */
     $app->get('/oauth/yandex', function($request, $response, $args) {
-        // // configration
-        // $clientId = getenv('YANDEX_CLIENT_ID');
-        // $redirectUri = getenv('APP_URL') . '/oauth/yandex';
+        // configration
+        $clientId = getenv('YANDEX_CLIENT_ID');
+        $redirectUri = getenv('APP_URL') . '/oauth/yandex';
 
-        // // get query params
-        // $params = $request->getQueryParams();
+        // get query params
+        $params = $request->getQueryParams();
 
-        // if (!empty($params['error'])) {
-        //     // Got an error, probably user denied access
-        //     exit('Got error: ' . $params['error']);
-        // } elseif (empty($params['code'])) {
-        //     // If we don't have an authorization code then get one
-        //     $scope = ['account-info', 'operation-history', 'operation-details', 'payment-p2p'];
-        //     $auth_url = API::buildObtainTokenUrl($clientId, $redirectUri, $scope);
-        //     // echo $auth_url;
-        //     header('Location: ' . $auth_url);
-        //     exit;
-        // } else {
-        //     $access_token_response = API::getAccessToken($clientId, $params['code'], $redirectUri, null);
-        //     $access_token = $access_token_response->access_token;
-        //     echo $access_token;
-        // }
+        if (!empty($params['error'])) {
+            // Got an error, probably user denied access
+            exit('Got error: ' . $params['error']);
+        } elseif (empty($params['code'])) {
+            // If we don't have an authorization code then get one
+            $scope = ['account-info', 'operation-history', 'operation-details', 'payment-p2p'];
+            $auth_url = API::buildObtainTokenUrl($clientId, $redirectUri, $scope);
+            // echo $auth_url;
+            header('Location: ' . $auth_url);
+            exit;
+        } else {
+            $access_token_response = API::getAccessToken($clientId, $params['code'], $redirectUri, null);
+            $access_token = $access_token_response->access_token;
+            echo $access_token;
+        }
     })->setName('oauth.yandex');
 
 ?>
