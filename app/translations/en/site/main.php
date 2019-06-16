c20f6081a11e26305537e9b1ac3464c5628110df
Your Name <you@example.com>
2019-06-16 17:43:45 +0400

    added main translation

---
+++ app/translations/en/site/main.php
@@ -0,0 +1,11 @@
+<?php 
+
+	return [
+		"locale" => "en",
+		"not_found" => "No results found",
+		"ok" => "Ok",
+		"next" => "Next",
+		"prev" => "Prev"
+	]; 
+
+?>
---
+++ app/translations/tr/site/main.php
@@ -0,0 +1,11 @@
+<?php 
+
+	return [
+		"locale" => "tr",
+		"not_found" => "Sonuç bulunamadı",
+		"ok" => "Ok",
+		"next" => "İleri",
+		"prev" => "Geri"
+	]; 
+
+?>
